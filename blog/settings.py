9f4ef2ab1257cb8c47217d06fe6db878df8b9026
RONI EFENDI <roniefendi0105@gmail.com>
2024-03-07 15:04:57 +0700

    ketujuh

--- blog/settings.py
+++ blog/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # STATICFILES_DIRS = os.path.join(BASE_DIR,"staticfiles_build"),
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles_build"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
